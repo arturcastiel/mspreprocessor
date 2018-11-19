d932eb33c8bd49ee2d323fd80e73d19ab5a38a42
Artur Castiel <arturcastiel@gmail.com>
2018-11-19 15:55:32 -0300

    skin

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import numpy as np
 from math import pi, sqrt
 from pymoab import core, types, rng, topo_util
+from pymoab import skinner as sk
+
 from meshUtil.meshManager import MeshManager as msh
+
 import numpy as np
+
 import pdb
 #%load_ext autoreload
 #%autoreload 2
@@ -19,17 +23,11 @@ a = np.random.choice(tf, int(2311))
 
 
 vec2 = vec[1:30]
-
-
-
-
-
-
 vec3 = np.array([1,3,4,2,5,2],dtype = 'uint')
 
 
-pdb.set_trace()
 rangeMod = M1.rangeIndex(vec2)
 
 data = np.roll(vec2,3)
 
+M1.print()
