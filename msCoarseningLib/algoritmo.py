973e91bf63dd2658bd9cf18c600d8c2d05ce1071
Artur Castiel <arturcastiel@gmail.com>
2018-11-21 00:32:22 -0300

    implementacao avancada do algoritmo 1

--- msCoarseningLib/algoritmo.py
+++ msCoarseningLib/algoritmo.py
@@ -6,9 +6,20 @@ def scheme1(msh,nx = 3, ny = 3, nz =3 ):
     centerCoord = msh.readData("CENTER")
     box = np.array([0,(msh.rx[1] -  msh.rx[0])/nx,
         0,(msh.ry[1] -  msh.ry[0])/ny, 0,(msh.rz[1] -  msh.rx[0])/nz]).reshape(3,2)
-
     cent_coord_El1 = box.sum(axis =1)/2
 
+    index = 0
+    for x in range(nx):
+
+        for y in range(ny):
+
+            for z in range(nz):
+                fla = np.array([nx*x,ny*y,nz*z,index])
+                pdb.set_trace()
+                f1 = fla[0:3] + box[:,0]
+                f2 = fla[0:3] + box[:,1]
+
+                index += 1
 
     print(centerCoord)
     pdb.set_trace()
