dcc623b83ecf231d941744f25029122b34f615a0
Artur Castiel <arturcastiel@gmail.com>
2018-11-22 12:41:17 -0300

    padronização das saidas dos algoritmos, criação de uma função para arrumar partições que não possuem celulas finas e precisam ser removidas

--- msCoarseningLib/algoritmo.py
+++ msCoarseningLib/algoritmo.py
@@ -1,45 +1,67 @@
 
-import pdb
+
 import numpy as np
-import pdb
+
+
+
+#para definir funções de particionamento deve-se atentar as saidas
+#é necessário retornar um vetor tag que associa uma partição a cada celula fina
+#também é necessario retornar o uma matrix
+# n x 3 com o centro de cada volume coarse, n -> numero de elementos do volume coarse
+
+#a função tagAdjust remove renumera os elementos da malha coarse de maneira que eles permanecam
+# continuos contando de 0 ao N de volumes coarse, a função tb remover os centros indesejados
+
+
 def scheme1(centerCoord, num_of_vol, rx,ry,rz ,nx = 3, ny = 3, nz =3 ):
     #input : centerCoord - > array with the center of elements
     #        num_of_vol = number of volumes
-    #        rx,ry,rz - (min, max) values of x,y,z
+    #        rx,ry,rz - (min, max) values of x,y,z of the phyisical domain
     #        nx, ny, nz
-
     # msh -> objeto da clase meshUtil
     #centerCoord = msh.readData("CENTER")
 
     box = np.array([0, (rx[1] - rx[0])/nx, 0,(ry[1] - ry[0]) /ny, 0,(rz[1] - rz[0])/(nz+0)]).reshape(3,2)
     cent_coord_El1 = box.sum(axis =1)/2
     tag = np.zeros(num_of_vol).astype("int")
+    coarseCenters = np.zeros((nx*ny*nz,3))
     index = 0
     for x in range(nx):
         for y in range(ny):
             for z in range(nz):
                 inc = np.multiply(box[:,1], np.array([x,y,z]))
-                cent = cent_coord_El1 + inc
+                #cent = cent_coord_El1 + inc
+                coarseCenters[index] = cent_coord_El1 + inc
                 #inc = np.array([(nx) * x, (ny) * y, (nz) * z])
                 boxMin = box[:,0] + inc
                 boxMax = box[:,1] + inc
                 point = checkinBox(centerCoord,x=(boxMin[0], boxMax[0]), y=(boxMin[1], boxMax[1]) , z=(boxMin[2], boxMax[2]))
                 tag[point] = index
                 index += 1
-    tagAdjust(tag)
-    return tag
+    print(coarseCenters)
+    return tagAdjust(tag,coarseCenters)
 
 
-def tagAdjust(tag):
-    # msh -> objeto da clase meshUtil
-    min = tag.min()
-    max = tag.max()
 
-
-    pdb.set_trace()
-
+#checa se um ponto esta dentro de um cubo
 def checkinBox(coords, x = (1,2), y = (2,3), z = (3,4)):
     tag1 = (coords[:,0] > x[0])   &  (coords[:,0] < x[1])
     tag2 = (coords[:,1] > y[0])   &  (coords[:,1] < y[1])
     tag3 = (coords[:,2] > z[0])   &  (coords[:,2] < z[1])
     return tag1 & tag2 & tag3
+
+
+
+#função para corrigir os tags em caso que as malhas geradoras possuam volumes grossos sem celulas dentro
+#e remover as respectivas coordenadas do centro dos volumes das malhas primais
+def tagAdjust(tag, coarseCenter):
+    # msh -> objeto da clase meshUtil
+    fineTag = np.zeros(len(tag))
+    elementsOriginal = [*set(tag)]
+    elementsNovo = [*set(range(len(elementsOriginal)))]
+    elementsMissing = set(range(len(coarseCenter))) - set(elementsOriginal)
+    for elo, eln in zip(elementsOriginal,elementsNovo):
+        if elo != eln:
+            pointer = (tag == elo)
+            fineTag[pointer] = eln
+    return fineTag.astype(int) , np.delete(coarseCenter, [*elementsMissing], axis = 0)
