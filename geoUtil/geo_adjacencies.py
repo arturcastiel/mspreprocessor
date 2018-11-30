05e08fca2839fef4101756cf55033ae91a55655d
Renata Tavares <renatattavares@hotmail.com>
2018-11-29 22:12:49 -0300

    Finalizada rotina para ordenamento dos pontos da pirâmide para o geometric module

--- geoUtil/geo_adjacencies.py
+++ geoUtil/geo_adjacencies.py
@@ -5,29 +5,38 @@ from pymoab import core, topo_util, rng
 
 mb = core.Core()
 mesh_topo_util = topo_util.MeshTopoUtil(mb)
+
 # Piramide
 
-def get_piramid_adjacencies(pi_nodes):
-    vertices = create_vertices(pi_nodes)
-    element = create_element(types.MBPYRAMID, vertices)
-    edges = mbcore.get_adjacencies(element, 1, True)
-    adjacencies = mesh_topo_util.get_bridge_adjacencies(vertices, 1, 0)
 
-base_nodes = rng.Range()
-top_node = rng.Range()
-i = 0
+def get_pyramid_adjacencies(vertices, edges):
 
-for i in range(0,len(adjacencies)):
-    if len(adjacencies[i]) == 3:
-        base_nodes.insert(vertices[i])
-    else:
-        top_node.insert(vertices[i])
+    # Creating used ranges
+    aux1 = rng.Range()
+    aux2 = rng.Range()
+    base_node = rng.Range()
+    top_node = rng.Range()
 
-aux = mesh_topo_util.get_bridge_adjacencies(base_nodes[0], 1, 0)
-coords1 = get_coords(base_nodes[0])
-coords2 = get_coords(aux[0])
-coords3 = get_coords(aux[1])
-coords4 = get_coords(top_node)
-order = np.array([coords1], [coords2], [coords3], [coords4])
+    # Discovering the top node of the pyramid
+    for e in vertex_handles:
+        adj = mesh_topo_util.get_bridge_adjacencies(e, 1, 0)
+        if len(adj) == 4:
+            top_node.insert(e)
 
-return parameter
+    # Defines the base nodes
+    base_nodes = rng.subtract(vertex_handles, top_node)
+
+    # Getting the base adjacencies of the point of reference
+    aux = mesh_topo_util.get_bridge_adjacencies(base_nodes[0], 1, 0)
+    aux = rng.subtract(top_node, aux)
+
+    aux1.insert(aux[0])
+    aux2.insert(aux[1])
+    base_node.insert(base_nodes[0])
+    coord1 = mbcore.get_coords(base_node)
+    coord2 = mbcore.get_coords(aux1)
+    coord3 = mbcore.get_coords(aux2)
+    coord4 = mbcore.get_coords(top_node)
+    order = ([coord1, coord2, coord3, coord4])
+
+    return order
